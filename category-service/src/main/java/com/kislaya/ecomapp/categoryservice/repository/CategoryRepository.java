394537e041d5f40a9953c59fa7e81505498e4487
KISLAYA AAKASH <kislayaaakash@gmail.com>
2022-11-29 10:39:08 +0530

    removed unwanted projects

--- category-service/src/main/java/com/kislaya/ecomapp/categoryservice/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kislaya.ecomapp.categoryservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kislaya.ecomapp.categoryservice.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-
-}
